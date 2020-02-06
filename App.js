fd24121b3ff06852f913821d476bcaf5a141e92d
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-02-06 13:01:29 +0100

    Extra changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Styles';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default function App() {
 	return (
@@ -10,11 +10,4 @@ export default function App() {
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+
